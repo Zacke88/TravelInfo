69e4f30cbfb1bfaea8ab352390647e554486e291
Zacke88 <zakrisson88@gmail.com>
2016-01-28 14:33:05 +0100

    combobox tråd

--- src/ComboBoxListener.java
+++ src/ComboBoxListener.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
  *
  * Lyssnare för val som görs i ComboBoxen som GUI't skapar
  * Denna combobox anger i vilket intervall tabellen skall updateras
+ * Efter ett val har gjorts ändrar den tiden för i vilket intervall tråden skall updatera tabellen med offers
  */
 public class ComboBoxListener implements ActionListener {
 
@@ -21,19 +22,13 @@ public class ComboBoxListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         if(e.getSource().toString().contains("30min")) {
-            System.out.println("30");
-            gui.setTimer(3*60*1000);
-            //gui.updater.start();
+            gui.setTimer(30*60*1000);
         }
         if(e.getSource().toString().contains("60min")) {
-            System.out.println("60");
             gui.setTimer(60*60*1000);
-            //gui.updater.start();
         }
         if(e.getSource().toString().contains("90min")) {
-            System.out.println("90");
             gui.setTimer(90*60*1000);
-            //gui.updater.start();
         }
     }
 }
