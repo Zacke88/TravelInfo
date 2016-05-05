b0673a33bafe22dccc9288b4743527ce20d7a6ea
Zacke88 <zakrisson88@gmail.com>
2016-05-05 12:21:10 +0200

    flyttat gui delar till swing worker done metod

--- src/BuildOfferTable.java
+++ src/BuildOfferTable.java
@@ -3,6 +3,8 @@ import org.xml.sax.SAXException;
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by id12jzn on 2016-03-09.
@@ -10,11 +12,13 @@ import java.io.IOException;
  * Denna klass anvands for att skapa eller updatera all data i tabellen.
  * Detta gors via en SwingWorker trad for att detta skall goras i bakgrunden
  * och inte paverka resten av programmet. Nar den har kort klart sa kallar
- * den pa builTable() i guit vilket sedan visar den nya tabellen for anvandaren
+ * den pa builTable() i guit genom Swing workers done metod vilket kör på EDT
+ * tråden som sedan visar den nya tabellen for anvandaren
  */
 public class BuildOfferTable extends SwingWorker<Void, Object> {
 
     private GUI gui;
+    private List offers = new ArrayList<ModelOffers>();
 
     public BuildOfferTable(GUI gui) {
         this.gui = gui;
@@ -33,9 +37,15 @@ public class BuildOfferTable extends SwingWorker<Void, Object> {
             gui.errorMessage.setText("<html><font-color 'red'>IO " +
                     "Error</font></html>");
         }
-        JTable table = new JTable(new OfferTable(xml.getList()));
-        gui.buildTable(table);
+        offers = xml.getList();
 
         return null;
+    }
+
+    @Override
+    protected void done() {
+        super.done();
+        JTable table = new JTable(new OfferTable(offers));
+        gui.buildTable(table);
     }
 }
